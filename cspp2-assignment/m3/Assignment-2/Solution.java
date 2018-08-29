76e7a2887320adbdf9c86fc0e96b191e976e965e
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-29 14:39:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m3/Assignment-2/Solution.java
+++ cspp2-assignment/m3/Assignment-2/Solution.java
@@ -16,7 +16,7 @@ public class Solution {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0,temp;
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             temp = i;
             while(temp > 0) {
                 if (temp % 10 == 7) {
