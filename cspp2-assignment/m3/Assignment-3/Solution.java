1cfe98179d35bfc22d10dabaacc18339407d004c
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-29 14:24:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m3/Assignment-3/Solution.java
+++ cspp2-assignment/m3/Assignment-3/Solution.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
