6219ee0c4da7d57cfd096ae52b1f683f747feb71
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-29 14:41:48 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m3/Assignment-3/Solution.java
+++ cspp2-assignment/m3/Assignment-3/Solution.java
@@ -1,19 +1,28 @@
-
 import java.util.Scanner;
-/*
-    Do not modify this main function.
-    */
+/**
+Do not modify this main function.
+*/
 public class Solution {
-
-    public static void main(String[] args) {
+    /**
+    *Do not modify this main function.
+    */
+    protected Solution() { }
+    /**
+    *Do not modify this main function.
+    *@param args is an argument
+    */
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         gcd(n1,n2);
         System.out.println(gcd(n1, n2));
         }
-    /*
-    Need to write the gcd function and print the output.
+    /**
+    *Need to write the power function and print the output.
+    *@param a is an argument
+    *@param b is an argument
+    *@return is an argument
     */
     public static int gcd(final int a, final int b) {
         if (a % b == 0) {
