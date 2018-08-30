c65eed103c3766c010a4b7feadc94107ae230ec7
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 15:24:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -9,24 +9,25 @@ public class Solution {
         int q = sc.nextInt();
         if ((m != p) || (n != q))
             System.out.println("not possible");                 
-        int a[][] = new int[m][n];
-        for (int i = 0; i<m; i++)
-            for (int j = 0; j<n; j++)
-                a[i][j] = sc.nextInt();
-        int b[][] = new int[p][q];
-        for (int i = 0; i<m; i++)
-            for (int j = 0; j<n; j++)
-                b[i][j] = sc.nextInt();
-        int c[][] = new int[m][n];
-        for (int i = 0; i<m; i++)
-            for (int j = 0; j<n; j++)
-                c[i][j] = a[i][j] + b[i][j];
-        for (int i = 0; i<m; i++){
-            for (int j = 0; j<n; j++){
-                System.out.print(c[i][j]+" ");
+        else;            
+            int a[][] = new int[m][n];
+            for (int i = 0; i<m; i++)
+                for (int j = 0; j<n; j++)
+                    a[i][j] = sc.nextInt();
+            int b[][] = new int[p][q];
+            for (int i = 0; i<m; i++)
+                for (int j = 0; j<n; j++)
+                    b[i][j] = sc.nextInt();
+            int c[][] = new int[m][n];
+            for (int i = 0; i<m; i++)
+                for (int j = 0; j<n; j++)
+                    c[i][j] = a[i][j] + b[i][j];
+            for (int i = 0; i<m; i++){
+                for (int j = 0; j<n; j++){
+                    System.out.print(c[i][j]+" ");
+                }
+                System.out.println( );
             }
-            System.out.println( );
-        }
 
 
 
