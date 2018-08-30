2466104f6a820cdcd00141fd193f3552d94f6c2d
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 15:09:37 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -4,12 +4,18 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        int n = sc.nextInt();        
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+        if (m != p)
+        	System.out.println("not possible");
+        if (n != q)
+        	System.out.println("not possible");       
         int a[][] = new int[m][n];
         for (int i = 0; i<m; i++)
         	for (int j = 0; j<n; j++)
         		a[i][j] = sc.nextInt();
-        int b[][] = new int[m][n];
+        int b[][] = new int[p][q];
         for (int i = 0; i<m; i++)
         	for (int j = 0; j<n; j++)
         		b[i][j] = sc.nextInt();
@@ -17,9 +23,12 @@ public class Solution {
         for (int i = 0; i<m; i++)
         	for (int j = 0; j<n; j++)
         		c[i][j] = a[i][j] + b[i][j];
-        for (int i = 0; i<m; i++)
-        	for (int j = 0; j<n; j++)
-        		System.out.print(c[i][j]);
+        for (int i = 0; i<m; i++){
+        	for (int j = 0; j<n; j++){
+        		System.out.print(c[i][j]+" ");
+        	}
+        	System.out.println( );
+        }
 
 
 
