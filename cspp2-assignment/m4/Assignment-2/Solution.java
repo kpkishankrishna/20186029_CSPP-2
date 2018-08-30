1d8c9ba85b112daaa7d35f8e2e7824651fd65252
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 15:53:02 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -5,32 +5,36 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
+        int a[][] = new int[m][n];
+        for (int i = 0; i<m; i++)
+            for (int j = 0; j<n; j++)
+                a[i][j] = sc.nextInt();
         int p = sc.nextInt();
         int q = sc.nextInt();
-        if ((m != p) || (n != q))
-            System.out.println("not possible");                 
-        else;            
-            int a[][] = new int[m][n];
-            for (int i = 0; i<m; i++)
-                for (int j = 0; j<n; j++)
-                    a[i][j] = sc.nextInt();
-            int b[][] = new int[p][q];
-            for (int i = 0; i<m; i++)
-                for (int j = 0; j<n; j++)
-                    b[i][j] = sc.nextInt();
-            int c[][] = new int[m][n];
-            for (int i = 0; i<m; i++)
-                for (int j = 0; j<n; j++)
-                    c[i][j] = a[i][j] + b[i][j];
-            for (int i = 0; i<m; i++){
-                for (int j = 0; j<n; j++){
+        int b[][] = new int[p][q];
+        for (int i = 0; i<m; i++)
+            for (int j = 0; j<n; j++)
+                b[i][j] = sc.nextInt();
+        int c[][] = new int[m][n];
+        for (int i = 0; i<m; i++)
+            for (int j = 0; j<n; j++)
+                c[i][j] = a[i][j] + b[i][j];
+        if ((m==p) && (n==q)) {
+            for (int i = 0; i<m; i++) 
+                for (int j = 0; j<n; j++) 
                     System.out.print(c[i][j]+" ");
-                }
-                System.out.println( );
-            }
+            
+            System.out.println( );
+        }
+
+        else {
+            System.out.println("not possible");
+        }
+
+        
+        }
+    }
 
 
 
         
-    }
-}
