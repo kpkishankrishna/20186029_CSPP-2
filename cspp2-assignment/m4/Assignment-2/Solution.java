dc3b07a6694c6a61e631bedb470e5449a847e36d
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 18:47:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -1,31 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
+/**
+*Matrices addition.
+*/
 public class Solution {
-    /* Fill the main function to print resultant of addition of matrices*/
-    public static void main(String[] args) {
+    /** 
+    *Fill the main function to print resultant of addition of matrices.
+    */
+    protected Solution() { }
+    /** 
+    *Fill the main function to print resultant of addition of matrices.
+    *@param args is argument.
+    */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
         int a[][] = new int[m][n];
-        for (int i = 0; i<m; i++)
-            for (int j = 0; j<n; j++)
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
                 a[i][j] = sc.nextInt();
         int p = sc.nextInt();
         int q = sc.nextInt();
         int b[][] = new int[p][q];
-        for (int i = 0; i<p; i++)
-            for (int j = 0; j<q; j++)
+        for (int i = 0; i < p; i++)
+            for (int j = 0; j < q; j++)
                 b[i][j] = sc.nextInt();
         if (m==p && n == q) {
             int c[][] = new int[m][n];
-            for (int i=0; i<m; i++) {
-                for (int j =0; j<n-1; j++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n - 1; j++) {
                     c[i][j] = a[i][j] + b[i][j];
                     System.out.print(c[i][j] + " ");
             }
-            c[i][n-1] = a[i][n-1] + b[i][n-1];
-            System.out.println(c[i][n-1]);
+            c[i][n - 1] = a[i][n - 1] + b[i][n - 1];
+            System.out.println(c[i][n - 1]);
         }
-    }    else{
+    }    else {
             System.out.println("not possible");
         }
     }
