bc44e13949f83fc68b99027e2ef3968683aff74d
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 18:21:47 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -21,9 +21,8 @@ public class Solution {
                 c[i][j] = a[i][j] + b[i][j];
         if ((m==n) && (p==q)) {
             for (int i = 0; i<m; i++) 
-                for (int j = 0; j<n; j++) {
+                for (int j = 0; j<n; j++) 
                     System.out.print(c[i][j]+" ");
-                }
                 System.out.print("\n");
 
             
