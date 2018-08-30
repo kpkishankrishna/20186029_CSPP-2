8bce46ff39ca06e47ba333b42bc202219a4a2f51
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 15:22:53 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-2/Solution.java
+++ cspp2-assignment/m4/Assignment-2/Solution.java
@@ -1,37 +1,35 @@
 import java.util.*;
 public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    /* Fill the main function to print resultant of addition of matrices*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
         int n = sc.nextInt();
         int p = sc.nextInt();
         int q = sc.nextInt();
-        if (m != p)
-        	System.out.println("not possible");
-        if (n != q)
-        	System.out.println("not possible");       
+        if ((m != p) || (n != q))
+            System.out.println("not possible");                 
         int a[][] = new int[m][n];
         for (int i = 0; i<m; i++)
-        	for (int j = 0; j<n; j++)
-        		a[i][j] = sc.nextInt();
+            for (int j = 0; j<n; j++)
+                a[i][j] = sc.nextInt();
         int b[][] = new int[p][q];
         for (int i = 0; i<m; i++)
-        	for (int j = 0; j<n; j++)
-        		b[i][j] = sc.nextInt();
+            for (int j = 0; j<n; j++)
+                b[i][j] = sc.nextInt();
         int c[][] = new int[m][n];
         for (int i = 0; i<m; i++)
-        	for (int j = 0; j<n; j++)
-        		c[i][j] = a[i][j] + b[i][j];
+            for (int j = 0; j<n; j++)
+                c[i][j] = a[i][j] + b[i][j];
         for (int i = 0; i<m; i++){
-        	for (int j = 0; j<n; j++){
-        		System.out.print(c[i][j]+" ");
-        	}
-        	System.out.println( );
+            for (int j = 0; j<n; j++){
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println( );
         }
 
 
 
-		
-	}
+        
+    }
 }
