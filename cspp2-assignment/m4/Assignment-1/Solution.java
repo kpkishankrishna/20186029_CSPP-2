43fd700c1b2f19abdad20e54834827d3a284b156
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 18:54:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-1/Solution.java
+++ cspp2-assignment/m4/Assignment-1/Solution.java
@@ -1,25 +1,28 @@
-
 import java.util.*;
-public class Solution
-{/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		int a[] = new int[n];
-		for(int i =0; i < a.length; i++) {
-			a[i] = sc.nextInt();
+/**
+* find the largest number.
+*/
+public class Solution {
+    /**
+    *Fill the main function to print resultant of addition of matrices.
+    */
+    protected Solution() { }
+    /**
+    *Fill the main function to print resultant of addition of matrices.
+    *@param args is argument.
+    */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int a[] = new int[n];
+        for(int i =0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        // System.out.println("//////");
+        Arrays.sort(a);
+        // System.out.println(Arrays.toString(a));
+        // System.out.println("............");
+        System.out.println(a[a.length-1]);
 
-		}
-		// System.out.println("//////");
-		
-		Arrays.sort(a);
-		// System.out.println(Arrays.toString(a));
-		// System.out.println("............");
-		System.out.println(a[a.length-1]);
-		
-
-	}
+    }
 }
