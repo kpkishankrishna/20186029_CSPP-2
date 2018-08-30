fea22406c88a3a6aa2587b2e46d3470792c8659d
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-30 14:20:59 +0530

    Average

---
+++ cspp2-practice/m4/Rough.java
@@ -0,0 +1,79 @@
+import java.util.*;
+class Rough {
+	public static void main(String[] args) {
+		// int arr[] = {10, 18, 32, 30, 24};
+		// for(int i = 0; i < arr.length; i++)
+		// 	if (arr[i]%3==0)
+		// 		System.out.println(arr[i]);
+		// int a[]= new int[-2];
+		// a[0]=0;
+		// a[-1]=-1;
+		// System.out.println(a[-1]);
+		// System.out.println(a[0]);
+		// System.out.println(a[2]);
+		// int a1[] = {1,2,3,4,5};
+		// a1[1]=11;
+		// System.out.println(a1[1]);
+		// int a2[] = {1,2,3};
+		// if (a1==a2)
+		// 	System.out.println("same");
+		// else
+		// 	System.out.println("not same");
+		// for(int i : a)
+			// System.out.println(a[i]);
+		// for(int element : a1)
+		// int i =3;
+		// for(i = 0; i<=4; i++)
+		// 	System.out.println(getMax(a1));
+		// int m = getMax(a1);
+		// System.out.println(m);
+		// int arr[] = new int[5];
+		// int arr[] = {1,5,3,8,2};
+		// int arr1[] = Collections.sort(arr);
+		// System.out.println(Arrays.toString(arr1));
+		// for (int i = 0;i<n ;i++ ) {
+		// 	System.out.print(a[i]);
+		// .......................................................
+		// int n;
+		// Scanner sc = new Scanner(System.in);
+		// n = sc.nextInt();
+		// int a[] = new int[n];
+		// for(int i =0; i < a.length; i++) {
+		// 	a[i] = sc.nextInt();
+
+		// }
+		// System.out.println("//////");
+		
+		// Arrays.sort(a);
+		// System.out.println(Arrays.toString(a));
+		// System.out.println("............");
+		// System.out.println(a[a.length-1]);
+		// ...........................................................
+		int a[][]={{1,2},{3,4}};
+		int b[][]= {{1,2},{3,4}};
+		int c[][]= new int[2][2];
+		// for(int i=0; i<2; )
+		// int m,n;
+		// Scanner sc = new Scanner(System.in);
+		// m = sc.nextInt();
+		// n = sc.nextInt();
+		// int a[][] = new int[m][n];
+		// for(int i=0; i<m; i++) {
+		// 	for(int j=0;j<n; j++) {
+		// 		a[i][j] = sc.nextInt();
+
+		// 	}
+		// }
+		// for(int i=0; i<m; i++) {
+		// 	for(int j=0;j<n; j++) {
+		// 		System.out.println(Arrays.toString(a[i][j]);
+		// 	}
+		System.out.println(Arrays.toString(a));
+
+
+
+		
+
+
+	}
+}
