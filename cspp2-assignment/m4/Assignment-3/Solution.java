f9ab0cca9bc346f57cc1fa4e7ed5c06e8f778101
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-31 16:02:49 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-3/Solution.java
+++ cspp2-assignment/m4/Assignment-3/Solution.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
-public class Solution
+public final class Solution
 {/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		for (int i = 0; i < n; i++){
-			String s = sc.next();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	static String binaryToDecimal(String s) {
-		int binary = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '1')
-				binary += (Math.pow(2,s.length() - i - 1));
-		}
-		return  (binary + "");
-			
-		}
-	}
+    Do not modify this main function.
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            String s = sc.next();
+            String res = binaryToDecimal(s);//Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    static String binaryToDecimal(final String s) {
+        int binary = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                binary += (Math.pow(2, s.length() - i - 1));
+            }
+        }
+        return  (binary + "");
+
+        }
+    }
