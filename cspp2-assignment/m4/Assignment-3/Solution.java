1dbe193c18ed0fbda0532c0a620b3b99bc2c7e7b
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-31 16:12:14 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-3/Solution.java
+++ cspp2-assignment/m4/Assignment-3/Solution.java
@@ -1,9 +1,27 @@
+/**
+*Do not modify this main function.
+*/
 import java.util.Scanner;
+/**
+*Do not modify this main function.
+*/
 public final class Solution {
+    protected Solution() { 
+        /**
+        *Do not modify this main function.
+        */}
     /**
     *Do not modify this main function.
     */
     public static void main(final String[] args) {
+        /**
+        *Fill the main function to print resultant of addition of matrices.
+        */
+        
+        /**
+        *Fill the main function to print resultant of addition of matrices.
+        *@param args is argument.
+        */
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
