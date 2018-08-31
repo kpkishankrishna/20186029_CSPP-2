976109fe5e13f7c465b2a9d7649b4844fca86bde
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-31 14:28:42 +0530

    concatinate

---
+++ cspp2-practice/m4/Concatinate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Concatinate {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String a = s.nextLine();
+		System.out.println(("Hello ").concat(a).concat("!"));
+
+	}
+}
