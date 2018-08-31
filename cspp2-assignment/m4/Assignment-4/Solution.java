e7a840744ecc45c540d89aedd00ca2cac7b0b8b1
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-31 16:23:13 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignment/m4/Assignment-4/Solution.java
+++ cspp2-assignment/m4/Assignment-4/Solution.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 *Do not modify this main function.
 */
 public class Solution {
-	/**
+    /**
     *Do not modify this main function.
     */
     protected Solution() {
@@ -18,28 +18,29 @@ public class Solution {
     *Do not modify this main function.
     *@param args is argument.
     *//*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	//Write reverseString function
-	/**
-	*Do not modify this main function.
-	*@param s is an argument
-	*@return is an argumanet
-	*/
+    Do not modify the main function 
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);  
+        System.out.println(reverse);
 
-	static String reverseString(String s) {
-		String reverse = "";
-		for(int i = 0; i < s.length(); i++)
-			reverse = s.charAt(i) + reverse;
-		return reverse;
+    }
+    //Write reverseString function
+    /**
+    *Do not modify this main function.
+    *@param s is an argument
+    *@return is an argumanet
+    */
 
-	}
+    static String reverseString(final String s) {
+        String reverse = "";
+        for (int i = 0; i < s.length(); i++) {
+            reverse = s.charAt(i) + reverse;
+        }
+        return reverse;
+
+    }
 
 }
