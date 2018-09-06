cf46afb0ba02b104629b059a63b6f99a77cdeb1e
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-09-06 11:17:49 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignment/m10/Assignment-1/List.java
+++ cspp2-assignment/m10/Assignment-1/List.java
@@ -283,10 +283,11 @@ public class List {
      */
     public void add(int index,int item) {
          // write the logic 
-        for (int i = size-1; i == index -1; i--) {
-            list[i+1] = list[i];
+        for (int i = size; i > index; i--) {
+            list[i] = list[i-1];
         }
         list[index] = item;
+        size++;
     }
     
     /* Returns the count of occurances of a given item in the list*/
