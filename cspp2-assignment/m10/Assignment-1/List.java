8661488918c7eda00d9d20e5bf8c754668fd7468
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-09-06 11:10:40 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignment/m10/Assignment-1/List.java
+++ cspp2-assignment/m10/Assignment-1/List.java
@@ -284,7 +284,7 @@ public class List {
     public void add(int index,int item) {
          // write the logic 
         for (int i = size-1; i > index; i--) {
-            list[i-1] = list[i];
+            list[i+1] = list[i];
         }
         list[index] = item;
     }
