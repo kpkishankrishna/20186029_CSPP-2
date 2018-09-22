f1c614cbc4827080d92b5145096f70c16e77d0ea
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-09-22 13:00:03 +0530

    "check_style_score -> 6 of 14 passed. style score: 0/1 "

--- cspp2-assignment/m25/Assignment-1/Task.java
+++ cspp2-assignment/m25/Assignment-1/Task.java
@@ -1,13 +1,44 @@
 class Task {
+	/**
+	 * tasktitle
+	 */
     private String tasktitle;
+    /**
+     * name
+     */
     private String nameofperson;
+    /**
+     * time
+     */
     private int minutes;
+    /**
+     * important
+     */
     private boolean important;
+    /**
+     * urgent
+     */
     private boolean urgent;
+    /**
+     * status
+     */
     private String status;
+    /**
+     * Constructs the object.
+     */
     Task() {
 
     }
+    /**
+     * 
+     *
+     * @param      tasktitle     The tasktitle
+     * @param      nameofperson  The nameofperson
+     * @param      minutes       The minutes
+     * @param      important     The important
+     * @param      urgent        The urgent
+     * @param      status        The status
+     */
     Task(String tasktitle, String nameofperson, int minutes, boolean important, boolean urgent, String status) throws Exception {
             if (tasktitle.equals("")) {
                 throw  new Exception("Title not provided");
@@ -34,15 +65,35 @@ class Task {
         }
 
     }
+    /**
+     * tasttitle
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String tasktitle () {
         return tasktitle;
     }
+    /**
+     * reurn name of person
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String nameofperson() {
         return nameofperson;
     }
+    /**
+     * return time
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int minutes() {
         return minutes;
     } 
+    /**
+     * return important
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String important() {
         String b = "";
         if (important) {
@@ -52,6 +103,11 @@ class Task {
         }
         return b;
     }
+    /**
+     * return urgent.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String urgent() {
         String a = "";
         if (urgent) {
@@ -61,9 +117,19 @@ class Task {
         }
         return a;
     }
+    /**
+     * reurn status.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public String status() {
         return status;
     }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         String a = "";
         String b = "";
