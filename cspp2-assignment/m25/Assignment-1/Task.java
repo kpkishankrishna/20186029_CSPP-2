646819c9b9b856b549f53f031da598f9486e1287
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-09-22 12:27:31 +0530

    "check_style_score -> 3 of 14 passed. style score: 0/1 "

--- cspp2-assignment/m25/Assignment-1/Task.java
+++ cspp2-assignment/m25/Assignment-1/Task.java
@@ -8,10 +8,7 @@ public class Task {
 	Task(String tasktitle, String nameofperson, int minutes, boolean important,
 		boolean urgent, String status) {
 		this.tasktitle = tasktitle;
-		if (tasktitle.equals("")) {
-			System.out.println("Title not provided");
-			throw new NullPointerException();
-		}
+
 		this.nameofperson = nameofperson;
 		this.minutes = minutes;
 		this.important = important;
@@ -19,6 +16,10 @@ public class Task {
 		this.status = status;
 	}
 	public String toString() {
+		if (tasktitle.equals("")) {
+			System.out.println("Title not provided");
+			throw new NullPointerException();
+		}
 		String one = "";
 		String two = "";
 		if (urgent){
