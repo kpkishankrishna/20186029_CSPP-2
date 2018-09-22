b3c4cb8523a9c1f8c1527c671b0db008dc9bc83b
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-09-22 11:42:14 +0530

    "check_style_score -> 0 of 14 passed. style score: 0/1 "

---
+++ cspp2-assignment/m25/Assignment-1/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+	private String tasktitle;
+	private String nameofperson;
+	private int minutes;
+	private boolean important;
+	private boolean urgent;
+	private String status;
+	Task(String tasktitle, String nameofperson, int minutes, boolean important,
+		boolean urgent, String status) {
+		this.tasktitle = tasktitle;
+		this.nameofperson = nameofperson;
+		this.minutes = minutes;
+		this.important = important;
+		this.urgent = urgent;
+		this.status = status;
+	}
+	public String toString() {
+		String one = "";
+		String two = "";
+		if (urgent){
+			one = "Urgent";
+		} else {
+			one = "Not Urgent";
+		}
+		if (important) {
+			two = "Important";
+		} else {
+			two = "Not Important";
+		}
+		return (tasktitle + ", " + nameofperson + ", " + minutes + ", " + one + ", " + two + ", " + status);
+
+	}
+
+}
